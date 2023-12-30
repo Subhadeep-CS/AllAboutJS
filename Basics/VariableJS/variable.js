5db65083a2f102697f01177a4005d1839e8a30be
subhadeep das <dassubhadeep631@gmail.com>
2023-12-30 20:03:28 +0530

    VariableJS folder added

---
+++ Basics/VariableJS/variable.js
@@ -0,0 +1,8 @@
+var x; //variable declrataion
+console.log(x); //JS engine store undefined in it.
+x=x+10; // we get NaN
+console.log(x);
+
+x=10; //variable initialization
+console.log(x);
+var y=10; //variable defination.
