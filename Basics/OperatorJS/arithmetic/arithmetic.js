aed9f8b94aa5650c26b58aa804ddd1e630f03e84
subhadeep das <dassubhadeep631@gmail.com>
2023-12-30 23:52:54 +0530

    First part of operator added

---
+++ Basics/OperatorJS/arithmetic/arithmetic.js
@@ -0,0 +1,26 @@
+var a=10;
+var b=2;
+
+//addition
+
+console.log(`Result of addition ${a+b}`);
+
+//subtraction
+
+console.log(`The result of subtraction ${a-b}`);
+
+//multiplication
+
+console.log(`The result of multiplication ${a*b}`);
+
+//division
+
+console.log(`The result of division ${a/b}`);
+
+//exponential
+
+console.log(`The result of exponential ${a**b}`);
+
+//modulo operator
+
+console.log(`The result of modulo ${a%b}`);
