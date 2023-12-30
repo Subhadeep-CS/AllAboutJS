01e15a39ebc3c3e858c0e25aa4ee971a234969d1
subhadeep das <dassubhadeep631@gmail.com>
2023-12-30 20:33:37 +0530

    DatatypeJS folder added

---
+++ Basics/DatatypeJS/Bigint.js
@@ -0,0 +1,4 @@
+console.log(Number.MAX_SAFE_INTEGER);
+
+var a=12345n;
+console.log(typeof a);
---
+++ Basics/DatatypeJS/datatype.js
@@ -0,0 +1,14 @@
+var x=10; //number type
+console.log(typeof x);
+var y=true;//boolean type
+console.log(typeof y);
+var a={}; //object type
+console.log(typeof a);
+var b=undefined; //undefined type 
+console.log(typeof b);
+var c=null; //null type but we get object here
+console.log(typeof c)
+var d=BigInt("10567"); //BigInt type
+console.log(typeof d);
+var e=Symbol("I am a boy");
+console.log(typeof e); //symbol type
